51d9156d24f59a33de35dda2e7456e12836d2c59
AlgoClassWork <kyxec.play@gmail.com>
2025-10-25 10:56:50 +0600

    Add files via upload

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+    slug = models.SlugField(max_length=100, unique=True, verbose_name='URL')
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, verbose_name='Категория')
+    name = models.CharField(max_length=100, verbose_name='Название')
+    slug = models.SlugField(max_length=100, unique=True, verbose_name='URL')
+    description = models.TextField(verbose_name='Описание')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    image = models.ImageField(upload_to='products/%Y/%m/%d/', verbose_name='Изображение')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    stock = models.PositiveIntegerField(verbose_name='Остаток на складе')
+    available = models.BooleanField(default=True , verbose_name='Доступен')
+
+    def __str__(self):
+        return self.name
