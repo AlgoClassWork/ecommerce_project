488762b7cb7ea4a08ae379c99bcbcbbf7307c9d6
AlgoClassWork <kyxec.play@gmail.com>
2025-11-01 10:38:23 +0600

    Update views.py

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import *
 
@@ -12,3 +12,8 @@ def product_list(request, slug=None):
 
     context = {'products':products, 'categories':categories}
     return render(request, 'product_list.html', context)
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, available=True)
+    context = {'product':product}
+    return render(request, 'product_detail.html', context)
